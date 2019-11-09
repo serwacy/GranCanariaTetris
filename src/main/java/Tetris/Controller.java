8776cbfbcfcff9af8bd76b822ab8afe0fb916c21
Bartek Matuszewski <bart.matusz@gmail.com>
2019-11-09 20:10:49 +0100

    Creating functioning credits button and scene
    horse

--- src/main/java/Tetris/Controller.java
+++ src/main/java/Tetris/Controller.java
@@ -1,4 +1,34 @@
 package Tetris;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+
+
 public class Controller {
+
+    @FXML
+    private Button creditsButton;
+
+    public void onButtonClick(ActionEvent event){
+        try {
+            if (event.getSource().equals(creditsButton)){
+                showCredits();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void showCredits() throws Exception {
+        Stage stage = (Stage) creditsButton.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Credits.fxml"));
+        stage.setScene(new Scene(root,600,800));
+        stage.show();
+    }
 }
