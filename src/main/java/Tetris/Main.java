c9b8f834b974cdbc12a753544db74dab5738c7f3
Bartek Matuszewski <bart.matusz@gmail.com>
2019-11-09 18:12:50 +0100

    Creation of basic main menu buttons
    no functionality yet

--- src/main/java/Tetris/Main.java
+++ src/main/java/Tetris/Main.java
@@ -1,7 +1,22 @@
 package Tetris;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Menu.fxml"));
+        primaryStage.setScene(new Scene(root,600,800));
+        primaryStage.setTitle("Gran Canaria Tetris - version 1.0");
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
         Game game = new Game();
+        launch(args);
     }
 }
