2b002b67984cb8362545af6101f4a887c6b64eb4
Bartek <bart.matusz@gmail.com>
2019-11-11 22:57:30 +0100

    adding game layout

--- src/main/java/Tetris/Controller.java
+++ src/main/java/Tetris/Controller.java
@@ -9,6 +9,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import sun.rmi.runtime.Log;
 
@@ -20,11 +21,15 @@ import java.util.*;
 public class Controller{
 
     @FXML
+    private Button playButton;
+    @FXML
     private Button creditsButton;
     @FXML
     private Button highScoresButton;
     @FXML
     private Button backButton;
+    @FXML
+    private Button stopButton;
     @FXML
     private Label highScores = new Label();
 
@@ -32,6 +37,9 @@ public class Controller{
     // ON BUTTON CLICK METHOD TO BE USED IN ANY CASE OF BUTTON EVENT IN OUR PROGRAM
     public void onButtonClick(ActionEvent event){
         try {
+            if (event.getSource().equals(playButton)){
+                showGame();
+            }
             if (event.getSource().equals(creditsButton)){
                 showCredits();
             }
@@ -41,11 +49,21 @@ public class Controller{
             if (event.getSource().equals(backButton)){
                 showMenu();
             }
+            if (event.getSource().equals(stopButton)){
+                fishGame();
+            }
         }catch (Exception e){
             e.printStackTrace();
         }
     }
     // BELOW ARE THE METHODS RESPONSIBLE FOR SCENE CHANGING - I KNOW IT IS CODE REPETITION - TO BE FIXED LATER
+    public void showGame() throws Exception {
+        Stage stage = (Stage) creditsButton.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Play.fxml"));
+        stage.setScene(new Scene(root,550,850));
+        stage.setResizable(false);
+        stage.show();
+    }
     public void showCredits() throws Exception {
         Stage stage = (Stage) creditsButton.getScene().getWindow();
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Credits.fxml"));
@@ -62,6 +80,13 @@ public class Controller{
     }
     public void showMenu() throws Exception {
         Stage stage = (Stage) backButton.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Menu.fxml"));
+        stage.setScene(new Scene(root,550,850));
+        stage.setResizable(false);
+        stage.show();
+    }
+    public void fishGame() throws Exception {
+        Stage stage = (Stage) stopButton.getScene().getWindow();
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Menu.fxml"));
         stage.setScene(new Scene(root,550,850));
         stage.setResizable(false);
--- src/main/java/Tetris/Main.java
+++ src/main/java/Tetris/Main.java
@@ -4,9 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
 
 public class Main extends Application {
     @Override
@@ -14,6 +18,8 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Menu.fxml"));
         primaryStage.setScene(new Scene(root,550,850));
         primaryStage.setTitle("Gran Canaria Tetris - version 1.0");
+
+        primaryStage.getIcons().add(new Image(new FileInputStream("./src/main/resources/images/horse_icon.jpg")));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
