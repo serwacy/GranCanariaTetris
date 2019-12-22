103f8b33f9f0a7f2a142171f1668a2f6afdd0c24
lumat <lumat@op.pl>
2019-12-22 23:31:43 +0100

    Save button
    onClick action returns to main menu

--- src/main/java/Controlers/SaveScoreController.java
+++ src/main/java/Controlers/SaveScoreController.java
@@ -1,9 +1,12 @@
 package Controlers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -11,10 +14,10 @@ public class SaveScoreController extends Controller implements Initializable {
     @FXML
     private Label finalScoreLabel;
 
+    @FXML
+    private Button saveButton;
 
-    public void setScoreLabel(int score) {
-        finalScoreLabel.setText(String.format("%04d", score));
-    }
+    PlayController playController;
 
 
     @Override
@@ -22,5 +25,25 @@ public class SaveScoreController extends Controller implements Initializable {
         PlayController playController = ControllerManager.getPlayController();
         int scoreValue = playController.getScoreValue();
         setScoreLabel(scoreValue);
+    }
+
+    public void setScoreLabel(int score) {
+
+        finalScoreLabel.setText(String.format("%04d", score));
+    }
+
+    @FXML
+    public void onButtonClick(ActionEvent event) {
+        try {
+            if (event.getSource().equals(saveButton)) {
+                showMenu();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showMenu() throws IOException {
+        prepareScene(saveButton, "Menu.fxml");
     }
 }
