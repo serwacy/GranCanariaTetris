5a784b0aab179ecdee51491a45b851a8d35cadd2
Grzegorz <serwacy2@gmail.com>
2020-01-03 21:27:05 +0100

    more preps for deployment
    working MusicPlayer

--- src/main/java/Services/MusicPlayer.java
+++ src/main/java/Services/MusicPlayer.java
@@ -1,24 +1,31 @@
 package Services;
 
 import javax.sound.sampled.*;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 public class MusicPlayer {
 
     public void play(){
-
-        try {
-            InputStream inputStream = this.getClass().getResourceAsStream("/sounds/korobeinikiSpanish.wav");
-//            File file = new File(this.getClass().getResource("/sounds/korobeinikiSpanish.wav").getFile());
-            Clip clip = AudioSystem.getClip();
-            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(inputStream);
-            clip.open(audioInputStream);
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        try (InputStream inputStream = getClass().getResourceAsStream("/sounds/korobeinikiSpanish.wav")) {
+            InputStream bufferedInputStream = new BufferedInputStream(inputStream);
+            try (AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(bufferedInputStream)) {
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioInputStream);
+                clip.start();
+            }
         } catch (LineUnavailableException | UnsupportedAudioFileException | IOException e) {
             e.printStackTrace();
         }
+
+//        try {
+//            InputStream inputStream = this.getClass().getResourceAsStream("/sounds/korobeinikiSpanish.wav");
+////            File file = new File(this.getClass().getResource("/sounds/korobeinikiSpanish.wav").getFile());
+//            Clip clip = AudioSystem.getClip();
+//            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(inputStream);
+//            clip.open(audioInputStream);
+//            clip.loop(Clip.LOOP_CONTINUOUSLY);
+//        } catch (LineUnavailableException | UnsupportedAudioFileException | IOException e) {
+//            e.printStackTrace();
+//        }
     }
 }
