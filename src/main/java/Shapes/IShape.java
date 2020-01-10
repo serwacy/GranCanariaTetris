7a432a58ecaed25cb7e27e73b9a8a661cc76d339
lumat <lumat@op.pl>
2020-01-10 23:07:13 +0100

    shape refactor
    BIMETHOG goes into action

--- src/main/java/Shapes/IShape.java
+++ src/main/java/Shapes/IShape.java
@@ -1,76 +1,43 @@
 package Shapes;
 
+import java.util.List;
+
 public class IShape extends Shape {
     public IShape(final String colorPath) {
         super(colorPath);
-        super.addBlocks(new Block(0,0, this.getColor()));
-        super.addBlocks(new Block(1,0, this.getColor()));
-        super.addBlocks(new Block(2,0, this.getColor()));
-        super.addBlocks(new Block(3,0, this.getColor()));
-        super.setRotation(1);
+        super.addBlocks(new Block(0, 0, this.color));
+        super.addBlocks(new Block(1, 0, this.color));
+        super.addBlocks(new Block(2, 0, this.color));
+        super.addBlocks(new Block(3, 0, this.color));
+        super.rotation = 1;
     }
 
-    public void rotate(Block[][] tetrion) {
-        if(canRotate(tetrion)) {
-            if (getRotation() == 1) {
-                translateRealBlockOfGivenShape(0, 2, -1);
-                translateRealBlockOfGivenShape(1, 1, 0);
-                translateRealBlockOfGivenShape(2, 0, 1);
-                translateRealBlockOfGivenShape(3, -1, 2);
-                super.setRotation(2);
-            } else if (getRotation() == 2) {
-                translateRealBlockOfGivenShape(0, 1, 2);
-                translateRealBlockOfGivenShape(1, 0, 1);
-                translateRealBlockOfGivenShape(2, -1, 0);
-                translateRealBlockOfGivenShape(3, -2, -1);
-                super.setRotation(3);
-            } else if (getRotation() == 3) {
-                translateRealBlockOfGivenShape(0, -2, 1);
-                translateRealBlockOfGivenShape(1, -1, 0);
-                translateRealBlockOfGivenShape(2, 0, -1);
-                translateRealBlockOfGivenShape(3, 1, -2);
-                super.setRotation(4);
-            } else if (getRotation() == 4) {
-                translateRealBlockOfGivenShape(0, -1, -2);
-                translateRealBlockOfGivenShape(1, 0, -1);
-                translateRealBlockOfGivenShape(2, 1, 0);
-                translateRealBlockOfGivenShape(3, 2, 1);
-                super.setRotation(1);
-            }
+    protected int calculateRotation(List<Block> blocks) {
+        if (rotation == 1) {
+            translateBlockOfGivenShape(blocks,0, 2, -1);
+            translateBlockOfGivenShape(blocks,1, 1, 0);
+            translateBlockOfGivenShape(blocks,2, 0, 1);
+            translateBlockOfGivenShape(blocks,3, -1, 2);
+            return  2;
+        } else if (rotation == 2) {
+            translateBlockOfGivenShape(blocks, 0, 1, 2);
+            translateBlockOfGivenShape(blocks, 1, 0, 1);
+            translateBlockOfGivenShape(blocks, 2, -1, 0);
+            translateBlockOfGivenShape(blocks, 3, -2, -1);
+            return  3;
+        } else if (rotation == 3) {
+            translateBlockOfGivenShape(blocks, 0, -2, 1);
+            translateBlockOfGivenShape(blocks, 1, -1, 0);
+            translateBlockOfGivenShape(blocks, 2, 0, -1);
+            translateBlockOfGivenShape(blocks, 3, 1, -2);
+            return  4;
+        } else if (rotation == 4) {
+            translateBlockOfGivenShape(blocks, 0, -1, -2);
+            translateBlockOfGivenShape(blocks, 1, 0, -1);
+            translateBlockOfGivenShape(blocks, 2, 1, 0);
+            translateBlockOfGivenShape(blocks, 3, 2, 1);
+            return  1;
         }
-    }
-
-    public boolean canRotate(Block[][] tetrion) {
-        initGhostBlocks();
-        if (getRotation() == 1) {
-            translateGhostBlockOfGivenShape(0, 2, -1);
-            translateGhostBlockOfGivenShape(1, 1, 0);
-            translateGhostBlockOfGivenShape(2, 0, 1);
-            translateGhostBlockOfGivenShape(3, -1, 2);
-        } else if (getRotation() == 2) {
-            translateGhostBlockOfGivenShape(0, 1, 2);
-            translateGhostBlockOfGivenShape(1, 0, 1);
-            translateGhostBlockOfGivenShape(2, -1, 0);
-            translateGhostBlockOfGivenShape(3, -2, -1);
-        } else if (getRotation() == 3) {
-            translateGhostBlockOfGivenShape(0, -2, 1);
-            translateGhostBlockOfGivenShape(1, -1, 0);
-            translateGhostBlockOfGivenShape(2, 0, -1);
-            translateGhostBlockOfGivenShape(3, 1, -2);
-        } else if (getRotation() == 4) {
-            translateGhostBlockOfGivenShape(0, -1, -2);
-            translateGhostBlockOfGivenShape(1, 0, -1);
-            translateGhostBlockOfGivenShape(2, 1, 0);
-            translateGhostBlockOfGivenShape(3, 2, 1);
-        }
-        if(getGhostBlocks().stream().anyMatch(x ->
-                x.getX() < 0 ||
-                x.getX() > tetrion.length -1 ||
-                x.getY() < 0 ||
-                x.getY() > tetrion[0].length -1)){
-            return false;
-        }
-        else return getGhostBlocks().stream().noneMatch(x ->
-                tetrion[x.getX()][x.getY()] != null);
+        return rotation;
     }
 }
--- src/main/java/Shapes/OShape.java
+++ src/main/java/Shapes/OShape.java
@@ -1,20 +1,19 @@
 package Shapes;
 
+import java.util.List;
+
 public class OShape extends Shape {
     public OShape(final String colorPath) {
         super(colorPath);
-        super.addBlocks(new Block(0, 0, this.getColor()));
-        super.addBlocks(new Block(1, 0, this.getColor()));
-        super.addBlocks(new Block(0, 1, this.getColor()));
-        super.addBlocks(new Block(1, 1, this.getColor()));
-        super.setRotation(1);
+        super.addBlocks(new Block(0, 0, this.color));
+        super.addBlocks(new Block(1, 0, this.color));
+        super.addBlocks(new Block(0, 1, this.color));
+        super.addBlocks(new Block(1, 1, this.color));
+        super.rotation = 1;
     }
 
-    public void rotate(Block[][] tetrion) {
-        //nothing happens
-    }
-
-    public boolean canRotate(Block[][] tetrion) {
-        return false;
+    @Override
+    protected int calculateRotation(final List<Block> blocks) {
+        return rotation;
     }
 }
